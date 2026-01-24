1c8f82c5228d7b17829214d22c3ed543e918648b
Wasilii92 <eup104st@yandex.ru>
2026-01-24 22:31:22 +0500

    Домашка выполнена

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,18 +1,18 @@
 package org.skypro.skyshop.product;
 
-public class Product {private final String name;
-    private final int price;
+public abstract class Product {
+    private String name;
 
-    public Product(String name, int price) {
+
+    public Product(String name) {
         this.name = name;
-        this.price = price;
+
     }
 
     public String getName(){
         return name;
     }
-    public int getPrice(){
-        return price;
-    }
-
+    public abstract int getPrice();
+    public abstract boolean isSpecial();
+    public abstract String toString();
 }
